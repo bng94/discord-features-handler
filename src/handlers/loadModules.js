6088c8cded6e0a6c469567a118ddd6c50a7a047b
Brandon Ng <14009481+bng94@users.noreply.github.com>
2022-06-12 14:03:29 -0400

    Update loadModules.js
    using map instead

--- src/handlers/loadModules.js
+++ src/handlers/loadModules.js
@@ -8,23 +8,26 @@ module.exports = (
   mainDirectory
 ) => {
   const loadModules = async (dir) => {
-    const dirname = dir.includes("../modules") ? __dirname : mainDirectory;
+    const builtInDirectory = dir.includes("../modules")
+    const dirname = builtInDirectory ? __dirname : mainDirectory;
     const files = fs.readdirSync(path.join(dirname, dir));
 
-    if (!dir.includes("../modules")) {
+    if (!builtInDirectory) {
       console.log(
         "[CMD]",
         ` Loading a total of ${files.length} modules in folder.`
       );
     }
 
-    for (const file of files) {
+    
+    const mapping = files.map((file) => {
       const stat = fs.lstatSync(path.join(dirname, dir, file));
+      console.log(file);
       if (stat.isDirectory()) {
         loadModules(path.join(dir, file));
       } else if (filesToExclude.includes(file) === false) {
         const feature = require(path.join(dirname, dir, file));
-        if (!dir.includes("../modules")) {
+        if (!builtInDirectory) {
           console.log(`Loading module file: ${file}`);
         }
         try {
@@ -36,14 +39,17 @@ module.exports = (
         console.log(`Modules File Excluded on load: ${file}`);
         return;
       }
-    }
+    })
+
+    Promise.all(mapping).then(()=> {
+      if (!builtInDirectory) console.log(`Modules Files Loaded`)
+    })
+
   };
 
   const loadingModules = directory.map(async (dirs) => {
     await loadModules(dirs);
   });
 
-  Promise.all(loadingModules).then(() => {
-    console.log("Finished loading all modules");
-  });
+  Promise.all(loadingModules);
 };
