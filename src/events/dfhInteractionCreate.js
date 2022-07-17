6195389df90068f54db35a2d23724aefd459b645
Brandon Ng <14009481+bng94@users.noreply.github.com>
2022-07-17 19:27:42 -0400

    log: updated console log
    - revisit some console log convention

--- src/events/dfhInteractionCreate.js
+++ src/events/dfhInteractionCreate.js
@@ -1,14 +1,14 @@
 module.exports = {
-    name: 'interactionCreate',
-    async execute(interaction, client) {
-        if (!interaction.isCommand()) return;
-        const { commandName, commandId  } = interaction;
-        const cmd = client.commands.get(commandName);
-        
-        console.log(`${commandName}`, `${interaction.user.tag}`, `SLASH CMD`);
-        console.log(commandId, 'ID', 'SLASH CMD');
-        
-        const level = client.getPermissionsLevel(interaction);
-        cmd.interactionReply(interaction, client, level);
-    },
+  name: "interactionCreate",
+  async execute(interaction, client) {
+    if (!interaction.isCommand()) return;
+    const { commandName, commandId } = interaction;
+    const cmd = client.commands.get(commandName);
+
+    console.log(`[SLASH CMD]`, `[${interaction.user.tag}]`, `${commandName}`);
+    console.log("[SLASH CMD]", "[ID]", commandId);
+
+    const level = client.getPermissionsLevel(interaction);
+    cmd.interactionReply(interaction, client, level);
+  },
 };
