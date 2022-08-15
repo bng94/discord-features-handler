858bbaa2c0cdcc227e83f02ed613f9bd29451651
Brandon Ng <14009481+bng94@users.noreply.github.com>
2022-08-14 23:01:05 -0400

    fix: improved interactionCreate event
    does not just return if is not a command, todo other interaction from discord.js

--- src/events/dfhInteractionCreate.js
+++ src/events/dfhInteractionCreate.js
@@ -1,14 +1,31 @@
 module.exports = {
   name: "interactionCreate",
   async execute(interaction, client) {
-    if (!interaction.isCommand()) return;
-    const { commandName, commandId } = interaction;
-    const cmd = client.commands.get(commandName);
+    if (interaction.isCommand()) {
+      const { commandName, commandId } = interaction;
+      const cmd = client.commands.get(commandName);
 
-    console.log(`[SLASH CMD]`, `[${interaction.user.tag}]`, `${commandName}`);
-    console.log("[SLASH CMD]", "[ID]", commandId);
+      console.log(`[SLASH CMD]`, `[${interaction.user.tag}]`, `${commandName}`);
+      console.log("[SLASH CMD]", "[ID]", commandId);
 
-    const level = client.getPermissionsLevel(interaction);
-    cmd.interactionReply(interaction, client, level);
+      const level = client.getPermissionsLevel(interaction);
+      cmd.interactionReply(interaction, client, level);
+    }
+    if (interaction.isButton()) {
+      //TODO: add interaction from cmd property
+      console.log("interaction button");
+    }
+    if (interaction.isAutocomplete()) {
+      //TODO: add interaction from cmd property
+      console.log("interaction isAutocomplete");
+    }
+    if (interaction.isContextMenu()) {
+      //TODO: add interaction from cmd property
+      console.log("interaction isContextMenu");
+    }
+    if (interaction.isModalSubmit()) {
+      //TODO: add interaction from cmd property
+      console.log("interaction isModalSubmit");
+    }
   },
 };
