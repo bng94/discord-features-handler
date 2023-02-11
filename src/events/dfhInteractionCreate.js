451b9d41570afb4165995868e9b57f41e3e65302
Brandon Ng <14009481+bng94@users.noreply.github.com>
2023-02-11 18:20:25 -0500

    feat: InteractionType.ApplicationCommand check added
    Removed interaction.isCommand() and other interactions with the type checking additional from v14. Also removed interaction .isButton(), .isContextMenu(), and .isUserContextMenu(), as right now haven't find the v14 version and not tested these functionality atm

--- src/events/dfhInteractionCreate.js
+++ src/events/dfhInteractionCreate.js
@@ -1,9 +1,10 @@
+const { InteractionType } = require("discord.js");
 module.exports = {
   name: "interactionCreate",
   async execute(interaction, client) {
     const { commandName, commandId, customId } = interaction;
     const level = client.getPermissionsLevel(interaction);
-    if (interaction.isCommand()) {
+    if (interaction.type === InteractionType.ApplicationCommand) {
       const cmd = client.commands.get(commandName);
 
       console.log(`[SLASH CMD]`, `[${interaction.user.tag}]`, `${commandName}`);
@@ -19,26 +20,7 @@ module.exports = {
       }
     }
 
-    /**
-     ** NOTE:
-     ** Since interactionReply return a command response, these modal/buttons may not, error may encounter (haven't fully tested all if else scenarios)
-     **
-     */
-    if (interaction.isButton()) {
-      //TODO: add interaction from cmd property
-      //* Similar to interactionReply, this should return the command
-      //* Since we need to check the id to ensure it matches the button action we are trying to execute, we need to filter here, since it may not match to the cmd file name (which it should in theory), it is easier to trace this method
-      const cmd = client.commands.filter(
-        (cmd) => cmd.buttonCustomId === customId
-      )[0];
-
-      try {
-        return cmd.buttonInteraction(interaction, client, level);
-      } catch (e) {
-        console.log("isButton interaction execution failed", e);
-      }
-    }
-    if (interaction.isAutocomplete()) {
+    if (interaction.type === InteractionType.ApplicationCommandAutocomplete) {
       const cmd = client.commands.get(commandName);
       try {
         return cmd.autoCompleteInteraction(interaction, client, level);
@@ -49,18 +31,7 @@ module.exports = {
         );
       }
     }
-    if (interaction.isContextMenu() || interaction.isUserContextMenu()) {
-      const cmd = client.commands.get(commandName);
-      try {
-        return cmd.contextMenuInteraction(interaction, client, level);
-      } catch (e) {
-        console.log(
-          "isCommand interaction (slash command) execution failed",
-          e
-        );
-      }
-    }
-    if (interaction.isModalSubmit()) {
+    if (interaction.type === InteractionType.ModalSubmit) {
       const cmd = client.commands.filter(
         (cmd) => cmd.modalCustomId === customId
       )[0];
