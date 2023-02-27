734f183d16ee5d3e3d98a32d71f807809ae67b4c
Brandon Ng <14009481+bng94@users.noreply.github.com>
2023-02-26 22:29:02 -0500

    Update dfhSlashCommands.js
    changed map to forEach and update guildId to be able to setup guild Id based slash commands (for development/testing)

--- src/events/dfhSlashCommands.js
+++ src/events/dfhSlashCommands.js
@@ -4,13 +4,13 @@ module.exports = {
   name: Events.ClientReady,
   once: true,
   async execute(client) {
-    const guildId = [client.config.serverID, ""];
+    const guildId = client.config.serverID ? client.config.serverID : "";
 
     const guild = client.guilds.cache.get(guildId);
     const commands = guild ? guild.commands : client.application?.commands;
 
     const cmds = client.commands;
-    const slashCmds = cmds.filter((cmd) => cmd.slash === true);
+    const slashCmds = cmds.filter((cmd) => cmd.slash === true || cmd.slashOnly);
 
     const cmdArray = [...slashCmds.values()];
 
@@ -20,7 +20,7 @@ module.exports = {
       `Loading a total of ${cmdArray.length} slash commands.`
     );
 
-    const mappingSlashCommands = cmdArray.map((cmd) => {
+    cmdArray.forEach((cmd) => {
       let tempObj = {
         name: cmd.name,
         description: cmd.description,
