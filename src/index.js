ce4cfede8302be5ffe3b2a6c7dec7198a7f66a8e
Brandon Ng <14009481+bng94@users.noreply.github.com>
2023-03-03 12:55:47 -0500

    Update index.js
    revisit the discord.js version checker

--- src/index.js
+++ src/index.js
@@ -122,7 +122,7 @@ const DiscordFeaturesHandler = async (
   /**
    * This assume node_modules folder is on same level at package.json, works well if the user is using an CI/CD environment.
    */
-  if (require("discord.js").version < "^14.7.0") {
+  if (require("discord.js").version.version.replace("^", "") < "14.7.0") {
     throw new Error(
       "Discord.js v14.7.0 or higher is required, you are using discord.js v" +
         require("discord.js").version.replace("^", "")
