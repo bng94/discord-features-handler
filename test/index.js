0c48b73d79f906aa3939c398381162ef025f9a5e
Brandon <14009481+bng94@users.noreply.github.com>
2024-04-22 18:31:52 -0400

    v2.0.1
    fixed multiple instances, not showing JSdoc in JS

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 const { Client, GatewayIntentBits, Partials } = require("discord.js");
-const DiscordFeaturesHandler = require("../src");
+const { DiscordFeaturesHandler } = require("../src");
 
 const client = new Client({
   intents: [
@@ -14,22 +14,15 @@ const client = new Client({
 });
 
 DiscordFeaturesHandler(client, {
+  config: "./config.js",
   directories: {
-    // file path to your script file
     main: __dirname,
   },
-  // file path and filename of your configuration file
-  config: "./config.js",
   onLoad_list_files: {
     commands: true,
     events: true,
     modules: true,
   },
-  // optional:
-  // files we want to excludes when handler is invoked
-  // files must be in their respective folders to work
-  // commands files in their sub folders
-  // events and modules files in their folder
   filesToExcludeInHandlers: {
     commands: ["ping.js"],
   },
